07d6da83d7a799c19e07fb93d84d1e4d7e68b686
Diiegote <diego.elprado22@gmail.com>
2024-03-17 14:24:46 -0300

    Servicio que se encarga de la logica para crear nuevas infos

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,11 @@
+const info = ["diego", "mama", "leiza", "termo", "mate"];
 
-
-export const getPruebaService = async () => {
-   const info = ["diego", "mama", "leiza", "termo", "mate"];
+export const getPruebaService = async () => {  
    return info;
+};
+
+export const postPruebaService = async (newData) => {
+   const createInfo = await getPruebaService();
+   createInfo.push(newData.data);
+   return createInfo;
 };
